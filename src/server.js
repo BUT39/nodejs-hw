4837e25075918ba329328b60529bd8db1e85a0ef
Dmytro But <butdmitrij25@mail.com>
2026-08-21 09:14:57 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(pinoHttp());
+app.use(pino());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
