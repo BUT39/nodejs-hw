e903116ef02be3a921753b313761aac2680cd74b
Dmytro But <butdmitrij25@mail.com>
2026-09-20 16:29:46 +0300

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,11 @@ export const updateUserAvatar = async (req, res) => {
     req.user._id.toString(),
   );
 
-  await User.findByIdAndUpdate(req.user._id, { avatar: result.secure_url });
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { returnDocument: 'after' },
+  );
 
-  res.status(200).json({ url: result.secure_url });
+  res.status(200).json({ url: updatedUser.avatar });
 };
